42c80270ab0d56d0024b537697b74f50649b6f39
banteg <banteeg@gmail.com>
2020-10-23 01:48:41 +0300

    add staked spank calculation

--- scripts/snapshot.py
+++ scripts/snapshot.py
@@ -6,11 +6,11 @@ from functools import wraps
 from pathlib import Path
 
 import toml
-from brownie import interface, web3
+from brownie import interface, web3, chain
 from eth_utils import event_abi_to_log_topic
 from hexbytes import HexBytes
-from toolz import groupby, valmap
-from tqdm import trange
+from toolz import groupby
+from tqdm import trange, tqdm
 
 spankbank_deploy = 6276045  # https://etherscan.io/tx/0xc6123eea98af9db149313005d9799eefd323baf1566adfaa53d25cc376229543
 uniswap_v1_deploy = 6627917  # https://etherscan.io/tx/0xc1b2646d0ad4a3a151ebdaaa7ef72e3ab1aa13aa49d0b7a3ca020f5ee7b1b010
@@ -22,6 +22,7 @@ def main():
     logs = fetch_logs()
     events = decode_logs(logs)
     calc_points(events)
+    calc_spank(events)
 
 
 def cached(path):
@@ -96,19 +97,55 @@ def decode_logs(logs):
 def calc_points(events):
     periods = defaultdict(dict)
     events = groupby("event", events)
-    print(events.keys())
-    print(valmap(len, events))
     for event in events["CheckInEvent"]:
         periods[event.args.period][event.args.staker] = event.args.spankPoints
     return dict(periods)
 
 
+@cached("snapshot/04-spank.json")
+def calc_spank(events):
+    periods = {}
+    events = groupby("event", events)
+    stakers = {event.args.staker for event in events["StakeEvent"]} | {
+        event.args.newAddress for event in events["SplitStakeEvent"]
+    }
+    print(len(stakers), "stakers")
+    end_time = chain[uni_deploy].timestamp
+    for period in range(spankbank.currentPeriod() + 1):
+        data = spankbank.periods(period)
+        period_end = data[5]
+        if period_end > end_time:
+            break
+        end_block = timestamp_to_block_number(period_end)
+        periods[period] = {
+            "snapshot_block": end_block,
+            "period_end": period_end,
+            "stakers": {},
+        }
+        print(f"period {period} snapshot block {end_block}")
+        for staker in tqdm(stakers):
+            # NOTE: the next line requires an archive node
+            spank_staked, *_ = spankbank.stakers(staker, block_identifier=end_block)
+            if spank_staked == 0:
+                continue
+            periods[period]["stakers"][staker] = spank_staked
+
+    return dict(periods)
+
+
+def timestamp_to_block_number(ts):
+    lo = 0
+    hi = chain.height
+    threshold = 1
+    while abs(hi - lo) > threshold:
+        mid = (hi - lo) // 2 + lo
+        if chain[mid].timestamp < ts:
+            lo = mid
+        else:
+            hi = mid
+    return hi
+
+
 def to_camel_case(snake_str):
     components = snake_str.split("_")
     return components[0] + "".join(x.title() for x in components[1:])
-
-
-def convert(log):
-    log = {to_camel_case(key): value for key, value in log.items()}
-    log["topics"] = [HexBytes(topic) for topic in log["topics"]]
-    return log
